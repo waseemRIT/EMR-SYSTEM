0861cb8cbbef6c9c71491eae20f75ed845cd193b
waseemRIT <whq8052@g.rit.edu>
2021-11-28 14:40:14 +0400

    1 a - DONE

---
+++ assignment3.py
@@ -0,0 +1,18 @@
+""" Assignment 3  -  Electronic Medical Record (EMR) """
+
+class Physician:
+
+    """ a - DONE """
+    __slots__ = ["id", "name", "__specialty"]
+
+    def __init__(self, id, name, specialty):
+        self.id = id
+        self.name = name
+        self.__specialty = specialty
+
+
+
+
+
+
+
