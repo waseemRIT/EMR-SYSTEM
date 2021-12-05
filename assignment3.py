ebb9d8cdabbc1ed34d492717fc4f3bf866a1ec87
waseemRIT <whq8052@g.rit.edu>
2021-12-05 20:32:20 +0400

    Final Version!!

--- assignment3.py
+++ assignment3.py
@@ -23,37 +23,37 @@ class Physician:
 
     ''' 1 b - DONE '''
 
-    # Returns id
+    # Returns Physician's id number
     def get_id(self):
         return self.__id
 
-    # Returns name
+    # Returns Physician's name
     def get_name(self):
         return self.__name
 
-    # Returns the speciality
+    # Returns the Physician's speciality
     def get_speciality(self):
         return self.__specialty
 
-    # Changes the id
+    # Changes the Physician's id number
     def set_id(self, new_id):
         self.__id = new_id
 
-    # Changes the name
+    # Changes the Physician's name
     def set_name(self, new_name):
         self.__name = new_name
 
-    # Changes the speciality
+    # Changes the Physician's speciality
     def set_specialty(self, new_speciality):
         self.__specialty = new_speciality
 
     ''' 1 c - DONE'''
 
-    # Returns the Physician's name
+    # Returns the Physician's name when Physician Object is pronted
     def __str__(self):
         return f"Physician: {self.__name}"
 
-    # Returns the Physicians specified attributes
+    # Returns the Physician's specified attributes
     def __repr__(self):
         return f"name: {self.__name}\nid: {self.__id}\nspeciality: {self.__specialty}\n"
 
@@ -82,41 +82,41 @@ class Patient:
 
     ''' 2 b - DONE'''
 
-    # returns emr_id
+    # returns Patient's emr_id number
     def get_emr_id(self):
         return self.__emr_id
 
-    # Returns name
+    # Returns Patient's name
     def get_name(self):
         return self.__name
 
-    # Returns gender
+    # Returns Patient's gender
     def get_gender(self):
         return self.__gender
 
-    # Returns Phone Number
+    # Returns Patient's Phone Number
     def get_phone_number(self):
         return self.__phone_number
 
-    # Changes emr_id
+    # Changes Patient's emr_id number
     def set_emr_id(self, new_emr_id):
         self.__emr_id = new_emr_id
 
-    # Changes name
+    # Changes the Patient's name
     def set_name(self, new_name):
         self.__name = new_name
 
-    # changes gender
+    # changes Patient's gender
     def set_gender(self, new_gender):
         self.__gender = new_gender
 
-    # changes phone number
+    # changes Patient's phone number
     def set_phone_number(self, new_phone_number):
         self.__phone_number = new_phone_number
 
     ''' 2 c - DONE'''
 
-    # print's the patient's name
+    # returns the patient's name when Patient Object is printed
     def __str__(self):
         return f"patient: {self.__name}"
 
@@ -125,6 +125,7 @@ class Patient:
         return f"emr_id{self.__emr_id}\nname: {self.__name}\ngender: {self.__gender}\nphone number: {self.__phone_number}\n "
 
 
+# 3 - DONE
 class Encounter:
     """
     Used to record the encounters between patients and physicians.
@@ -147,15 +148,15 @@ class Encounter:
         self.patient = patient
         self.physician = physician
 
-    # returns physician name
+    # returns physician's name
     def get_physician(self):
         return self.physician.get_name()
 
-    # returns patient name
+    # returns patient's name
     def get_patient(self):
         return self.patient.get_name()
 
-    # returns the date name
+    # returns the date of the encounter
     def get_date(self):
         return self.date
 
@@ -172,6 +173,7 @@ class Encounter:
         return f"{self.physician}\n{self.patient}"
 
 
+# Used to add the Encounters in a CSV File
 def add_encounter_csv(physician, patient, date, disease, medication):
     with open("encounter.csv", "a", newline="") as encounter:
         # A Tuple of my fields to add
@@ -180,25 +182,78 @@ def add_encounter_csv(physician, patient, date, disease, medication):
         writer.writerow(fields)
 
 
+# Returns the Physician's attributes from a Physicians csv File
+def physician_retriever(csv_file):
+    all_physicians = []
+    with open(csv_file) as physicians:
+        physicians = csv.reader(physicians)
+        for attributes in physicians:
+            all_physicians.append(attributes)
+
+    return all_physicians
+
+
+# Returns the Patient's attributes from a Patients csv File
+def patients_retriever(csv_file):
+    all_patients = []
+    with open(csv_file) as patient:
+        patients = csv.reader(patient)
+        for attributes in patients:
+            all_patients.append(attributes)
+
+    return all_patients
+
+
 def main():
-    # The Physicians
-    physician1 = Physician(12, "Waseem", "Neurologists")
-    physician2 = Physician(13, "Kassem", "Anesthesiology")
-    physician3 = Physician(14, "Mazen", "Dermatology")
+    # will store all the physicians here
+    physicians = physician_retriever(r"physicians.csv")
 
-    # The Patients
-    patient1 = Patient(1, "wael", "male", 971561053678)
-    patient2 = Patient(2, "Rami", "male", 971561053693)
-    patient3 = Patient(3, "Rama", "female", 971561053628)
-    patient4 = Patient(4, "Jude", "female", 971561034678)
-    patient5 = Patient(5, "Wakeel", "male", 971561053267)
-    patient6 = Patient(6, "Osama", "male", 971561053378)
-    patient7 = Patient(7, "Afra", "female", 971526753678)
-    patient8 = Patient(8, "Rashid", "male", 971561278678)
-    patient9 = Patient(9, "Mustafa", "male", 971561028678)
-    patient10 = Patient(10, "Osama", "male", 971518053678)
+    # the attributes of all physicians
+    headers, physician1_atr, physician2_atr, physician3_atr = physicians
 
-    # The Encounters
+    # the attributes of each physicians separated
+    id1, name1, speciality1, _ = physician1_atr  # physician 1 attributes
+    id2, name2, speciality2, _ = physician2_atr  # physician 2 attributes
+    id3, name3, speciality3, _ = physician3_atr  # physician 3 attributes
+
+    # Defining The Physicians
+    physician1 = Physician(id1, name1, speciality1)
+    physician2 = Physician(id2, name2, speciality2)
+    physician3 = Physician(id3, name3, speciality3)
+
+    # will store all patients here
+    patients = patients_retriever(r"patients.csv")
+
+    # The attributes of all patients
+    headers, patient1, patient2, patient3, patient4, patient5, \
+    patient6, patient7, patient8, patient9, patient10 = patients
+
+    # the attributes of each physicians separated
+
+    emr_id1, pname1, gender1, phone_num1 = patient1  # Patient 1 attributes
+    emr_id2, pname2, gender2, phone_num2 = patient2  # Patient 2 attributes
+    emr_id3, pname3, gender3, phone_num3 = patient3  # Patient 3 attributes
+    emr_id4, pname4, gender4, phone_num4 = patient4  # Patient 4 attributes
+    emr_id5, pname5, gender5, phone_num5 = patient5  # Patient 5 attributes
+    emr_id6, pname6, gender6, phone_num6 = patient6  # Patient 6 attributes
+    emr_id7, pname7, gender7, phone_num7 = patient7  # Patient 7 attributes
+    emr_id8, pname8, gender8, phone_num8 = patient8  # Patient 8 attributes
+    emr_id9, pname9, gender9, phone_num9 = patient9  # Patient 9 attributes
+    emr_id10, pname10, gender10, phone_num10 = patient10  # Patient 10 attributes
+
+    # The Patients Objects Creation
+    patient1 = Patient(emr_id1, pname1, gender1, phone_num1)
+    patient2 = Patient(emr_id2, pname2, gender2, phone_num2)
+    patient3 = Patient(emr_id3, pname3, gender3, phone_num3)
+    patient4 = Patient(emr_id4, pname4, gender4, phone_num4)
+    patient5 = Patient(emr_id5, pname5, gender5, phone_num5)
+    patient6 = Patient(emr_id6, pname6, gender6, phone_num6)
+    patient7 = Patient(emr_id7, pname7, gender7, phone_num7)
+    patient8 = Patient(emr_id8, pname8, gender8, phone_num8)
+    patient9 = Patient(emr_id9, pname9, gender9, phone_num9)
+    patient10 = Patient(emr_id10, pname10, gender10, phone_num10)
+
+    # The Encounters Objects Creation
     encounter1 = Encounter(physician1, patient7, "13/11/21", "anxiety", "Xanax")
     encounter2 = Encounter(physician2, patient4, "14/08/21", "face rash", "Calamine")
     encounter3 = Encounter(physician1, patient3, "11/04/21", "diabetes", "Linagliptin")
@@ -206,6 +261,9 @@ def main():
     encounter5 = Encounter(physician2, patient9, "04/08/21", "high cholesterol", "Atorvastatin")
 
     # printing the patients
+
+    print("Patients:-> \n")
+
     print(patient1)
     print(patient2)
     print(patient3)
@@ -218,7 +276,9 @@ def main():
     print(patient10)
 
     # to differentiate between patients and physicians
-    print("#########################################################")
+    print("#########################################################\n")
+
+    print("Physicians:-> \n")
 
     # printing the Physicians
     print(physician1)
@@ -226,13 +286,17 @@ def main():
     print(physician3)
 
     # to differentiate between Encounters and the others
-    print("#########################################################")
-    print(encounter1)
-    print(encounter2)
-    print(encounter3)
-    print(encounter4)
+    print("#########################################################\n")
+
+    print("Encounters:->\n")
+
+    # Printing the Encounters
+    print(encounter1,"\n")
+    print(encounter2,"\n")
+    print(encounter3,"\n")
+    print(encounter4,"\n")
     print(encounter5)
-    
+
     ''' To Add Encounters into the Encounters.csv File '''
 
     # I used this to add the headers, since I am appending in the function, so this will help in not rewriting the
@@ -244,7 +308,8 @@ def main():
         writer.writerow(headers)
 
     # Adds Encounter 1
-    add_encounter_csv(encounter1.get_physician(), encounter1.get_patient(), encounter1.get_date(), encounter1.get_disease(), encounter1.get_medication())
+    add_encounter_csv(encounter1.get_physician(), encounter1.get_patient(), encounter1.get_date(),
+                      encounter1.get_disease(), encounter1.get_medication())
 
     # Adds Encounter 2
     add_encounter_csv(encounter2.get_physician(), encounter2.get_patient(), encounter2.get_date(),
