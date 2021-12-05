3cb543c87e2d67659c946f3e1720a8237ffdbbd2
waseemRIT <whq8052@g.rit.edu>
2021-12-05 19:24:47 +0400

    Done!!

--- assignment3.py
+++ assignment3.py
@@ -1,10 +1,11 @@
+import csv
+
 """ Assignment 3  -  Electronic Medical Record (EMR) """
 
 
 class Physician:
     """
-    A Physician Class
-    A physician has
+    A Physician Class That Contains:
     ID --> integer
     Name --> string
     Speciality --> string
@@ -47,9 +48,10 @@ class Physician:
         self.__specialty = new_speciality
 
     ''' 1 c - DONE'''
+
     # Returns the Physician's name
     def __str__(self):
-        return f"Physician {self.__name}"
+        return f"Physician: {self.__name}"
 
     # Returns the Physicians specified attributes
     def __repr__(self):
@@ -79,6 +81,7 @@ class Patient:
         self.__emr_id = emr_id
 
     ''' 2 b - DONE'''
+
     # returns emr_id
     def get_emr_id(self):
         return self.__emr_id
@@ -115,7 +118,7 @@ class Patient:
 
     # print's the patient's name
     def __str__(self):
-        return f"patient {self.__name}"
+        return f"patient: {self.__name}"
 
     # print's the patients full details
     def __repr__(self):
@@ -146,19 +149,35 @@ class Encounter:
 
     # returns physician name
     def get_physician(self):
-        return self.physician
+        return self.physician.get_name()
 
+    # returns patient name
     def get_patient(self):
-        return self.patient
+        return self.patient.get_name()
 
+    # returns the date name
     def get_date(self):
         return self.date
 
+    # returns the disease type
     def get_disease(self):
         return self.disease
 
+    # returns the medication
     def get_medication(self):
         return self.medication
+
+    # print's Patient's and Physician's names
+    def __str__(self):
+        return f"{self.physician}\n{self.patient}"
+
+
+def add_encounter_csv(physician, patient, date, disease, medication):
+    with open("encounter.csv", "a", newline="") as encounter:
+        # A Tuple of my fields to add
+        fields = (physician, patient, date, disease, medication)
+        writer = csv.writer(encounter)
+        writer.writerow(fields)
 
 
 def main():
@@ -173,18 +192,18 @@ def main():
     patient3 = Patient(3, "Rama", "female", 971561053628)
     patient4 = Patient(4, "Jude", "female", 971561034678)
     patient5 = Patient(5, "Wakeel", "male", 971561053267)
-    patiient6 = Patient(6, "Osama", "male", 971561053378)
+    patient6 = Patient(6, "Osama", "male", 971561053378)
     patient7 = Patient(7, "Afra", "female", 971526753678)
     patient8 = Patient(8, "Rashid", "male", 971561278678)
     patient9 = Patient(9, "Mustafa", "male", 971561028678)
     patient10 = Patient(10, "Osama", "male", 971518053678)
 
     # The Encounters
-    encount1 = Encounter(physician1, patient7, "13/11/21", "anxiety", "Xanax")
-    encount2 = Encounter(physician2, patient4, "14/08/21", "face rash", "Calamine")
-    encount3 = Encounter(physician1, patient3, "11/04/21", "diabetes", "Linagliptin")
-    encount4 = Encounter(physician3, patient1, "06/010/21", "high blood pressure", "Diuretics")
-    encount5 = Encounter(physician2, patient9, "04/08/21", "high cholesterol", "Atorvastatin")
+    encounter1 = Encounter(physician1, patient7, "13/11/21", "anxiety", "Xanax")
+    encounter2 = Encounter(physician2, patient4, "14/08/21", "face rash", "Calamine")
+    encounter3 = Encounter(physician1, patient3, "11/04/21", "diabetes", "Linagliptin")
+    encounter4 = Encounter(physician3, patient1, "06/010/21", "high blood pressure", "Diuretics")
+    encounter5 = Encounter(physician2, patient9, "04/08/21", "high cholesterol", "Atorvastatin")
 
     # printing the patients
     print(patient1)
@@ -192,7 +211,7 @@ def main():
     print(patient3)
     print(patient4)
     print(patient5)
-    print(patiient6)
+    print(patient6)
     print(patient7)
     print(patient8)
     print(patient9)
@@ -205,6 +224,43 @@ def main():
     print(physician1)
     print(physician2)
     print(physician3)
+
+    # to differentiate between Encounters and the others
+    print("#########################################################")
+    print(encounter1)
+    print(encounter2)
+    print(encounter3)
+    print(encounter4)
+    print(encounter5)
+    
+    ''' To Add Encounters into the Encounters.csv File '''
+
+    # I used this to add the headers, since I am appending in the function, so this will help in not rewriting the
+    # headers every time the function is called
+    with open("encounter.csv", "a", newline="") as encounter:
+        # Tuple of headers
+        headers = ("Physician", "Patient", "Date", "Disease", "Medication")
+        writer = csv.writer(encounter)
+        writer.writerow(headers)
+
+    # Adds Encounter 1
+    add_encounter_csv(encounter1.get_physician(), encounter1.get_patient(), encounter1.get_date(), encounter1.get_disease(), encounter1.get_medication())
+
+    # Adds Encounter 2
+    add_encounter_csv(encounter2.get_physician(), encounter2.get_patient(), encounter2.get_date(),
+                      encounter2.get_disease(), encounter2.get_medication())
+
+    # Adds Encounter 3
+    add_encounter_csv(encounter3.get_physician(), encounter3.get_patient(), encounter3.get_date(),
+                      encounter3.get_disease(), encounter3.get_medication())
+
+    # Adds Encounter 4
+    add_encounter_csv(encounter4.get_physician(), encounter4.get_patient(), encounter4.get_date(),
+                      encounter4.get_disease(), encounter4.get_medication())
+
+    # Adds Encounter 5
+    add_encounter_csv(encounter5.get_physician(), encounter5.get_patient(), encounter5.get_date(),
+                      encounter5.get_disease(), encounter5.get_medication())
 
 
 main()
